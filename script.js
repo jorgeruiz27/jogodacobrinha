8f43af8e257d898da4e95d1ce24c08adb8a5ddd6
jorgeruiz27 <jorge.cx.27@gamil.com>
2022-04-27 22:57:22 -0400

    criar limite tela

--- script.js
+++ script.js
@@ -20,14 +20,30 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown' , update);
+
+function update (event){
+    
+    if(event.keyCode == 37 && direction != "rigth") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+    if(event.keyCode == 39 && direction != "left") direction = "rigth";
+}
+
 function iniciarJogo(){
+
+    if(snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "right") snakeX += box;
+    if(direction == "rigth") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
